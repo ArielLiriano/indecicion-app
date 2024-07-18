5bd7be3b524be656ab3132fb026054d8315a6b38
José Ariel Liriano <52571899+ArielLiriano@users.noreply.github.com>
2024-07-18 19:10:39 -0400

    Inicio de indecicion app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <hr />
+  <MyCounter :value="10" />
+  <MyCounterScript :value="5" />
+  <MyCounterScript2 :value="5" />
+</template>
+
+<script lang="ts" setup>
+import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+</script>
